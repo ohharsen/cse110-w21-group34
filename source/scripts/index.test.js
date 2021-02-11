1d0aa86970cfaf64c04ac0011a332a563040f5f5
virenabhyankar <forvirenra@gmail.com>
2021-02-11 12:29:39 -0800

    Remove window load

--- source/scripts/index.test.js
+++ source/scripts/index.test.js
@@ -5,21 +5,15 @@
 let { togglePomoBreak, startTimer, resetTimer } = require("./index");
 
 test('checks break toggle', () => {
-  window.onload = function() {
-    expect(togglePomoBreak(true)).toBe(false);
-    expect(togglePomoBreak(false)).toBe(true);
-  }
+  expect(togglePomoBreak(true)).toBe(false);
+  expect(togglePomoBreak(false)).toBe(true);
 });
   
 test('checks start state', () => {
-  window.onload = function() {
-    expect(startTimer().toBe(["pomo","✖ Reset"]));
-  }
+  expect(startTimer().toBe(["pomo","✖ Reset"]));
 });
   
 test('checks reset state', () => {
-  window.onload = function() {
-    expect(resetTimer().toBe(["stopped","▶ Begin"]));
-  }
+  expect(resetTimer().toBe(["stopped","▶ Begin"]));
 });
 
