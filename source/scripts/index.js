21d0770355511b8a19bc157d3fb5f323ad697dc4
virenabhyankar <forvirenra@gmail.com>
2021-02-10 23:48:42 -0800

    Add variables and functions

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,40 @@
-/**
- * Sum of two numbers
- * @param {int} a First operand
- * @param {int} b Second operand
- * @returns their sum
- */
-function sum(a, b) {
-    return a + b;
-}  
+// Constants
+const START_STOP_ID = "start-stop-button";
+const RESET_BTN_TXT = "✖ Reset";
+const BEGIN_BTN_TXT = "▶ Begin";
 
-module.exports = sum;
+// Enum for storing pomo state -- feel free to change
+const timerOptions = {
+    STOPPED: "stopped",
+    POMO: "pomo",
+    SHORT: "short break",
+    LONG: "long break"
+}
+
+let startStopButton = document.getElementById(START_STOP_ID);
+startStopButton.classList.toggle("break-button");
+let pomoState = timerOptions.STOPPED;
+
+startStopButton.addEventListener("click", function() {
+    if (pomoState == timerOptions.STOPPED) {
+        startTimer();
+    } else {
+        resetTimer();
+    }
+});
+
+function togglePomoBreak() {
+    startStopButton.classList.toggle("break-button");
+}
+
+function startTimer() {
+    pomoState = timerOptions.POMO;
+    startStopButton.innerHTML = RESET_BTN_TXT;
+}
+
+function resetTimer() {
+    pomoState = timerOptions.STOPPED;
+    startStopButton.innerHTML = BEGIN_BTN_TXT;
+}
+
+module.exports = togglePomoBreak, startTimer, resetTimer;
