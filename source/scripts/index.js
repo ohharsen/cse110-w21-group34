dc416933a9ffe3114997ffc1cc38f8897e8b1395
Keshab Agarwal <keagarwa@ucsd.edu>
2021-02-16 11:08:40 -0800

    Timer Animation done

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,17 @@
+// import { beginCountdown, stdWork, stdBreak, stdExtBreak, beginBreak, pomoCount} from './buttonTest.js';
+
+//Sets the color of the timer
+window.onload = function(){
+document.getElementById("base-timer-path-remaining").setAttribute("stroke", "#DB2E2E");
+
+// variables from buttonTest.js
+var isShown = true;
+var onBreak = false;
+var pomoCount = 0;
+var stdWork = 10; // 1500
+var stdBreak = 3; // 300
+var stdExtBreak = 8; // 900
+
 /* Constants */
 const START_STOP_ID = "start-stop-button";
 const RESET_BTN_TXT = "✖ Reset";
@@ -44,9 +58,28 @@ function togglePomoBreak(onBreak) {
  * @todo timer-specific functionality
  */
 function startTimer() {
-    pomoState = timerOptions.POMO;
     if (startStopButton) { 
         startStopButton.innerHTML = RESET_BTN_TXT;
+
+        // Copied from buttonTest
+        var display = document.querySelector('#countdownText');
+        if (onBreak == false) {
+            pomoState = timerOptions.POMO;
+            beginCountdown(stdWork, display);
+        }
+        else {
+            if(pomoCount == 3) {
+                pomoCount = 0;
+                pomoState = timerOptions.LONG;
+                beginBreak(stdExtBreak, display);
+            }
+            else {
+                pomoCount++;
+                pomoState = timerOptions.SHORT;
+                beginBreak(stdBreak, display);
+            }
+        }
+        //
     }
     return [pomoState, RESET_BTN_TXT];
 }
@@ -63,4 +96,94 @@ function resetTimer() {
     return [pomoState, BEGIN_BTN_TXT];
 }
 
+// --------------------- Copied from buttonTest -------------------------
+
+// Main countdown function.
+function beginCountdown(duration, textDisplay) {
+	let timer = duration; // minutes, seconds;
+
+    var interval = setInterval(function() {
+    currentTime(timer, textDisplay);
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", `${(timeFraction(timer, onBreak) * 220)} 220`);
+   
+    // Press break in middle of countdown.
+    if (pomoState == timerOptions.STOPPED) {
+        clearInterval(interval);
+        pomoCount = 0;
+        onBreak = false;
+        currentTime(stdWork, textDisplay);
+        document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", `220 220`);
+    }    
+    
+    if (--timer < -1) { 
+        document.getElementById("base-timer-path-remaining").setAttribute("stroke", "#34DBB3");
+        clearInterval(interval);
+        onBreak = true;
+        startStopButton.innerHTML = BEGIN_BTN_TXT; 
+        pomoState = timerOptions.STOPPED;
+        if(pomoCount == 3) {
+            currentTime(stdExtBreak, textDisplay);
+        }
+        else {
+            currentTime(stdBreak, textDisplay);
+        }
+    }
+  }, 1000);
+}
+
+// Main break function.
+function beginBreak(duration, textDisplay) {
+	let timer = duration; // minutes, seconds;
+    let interval = setInterval(function() {
+    currentTime(timer, textDisplay);
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", `${(timeFraction(timer) * 220)} 220`);
+    
+    // Press break in middle of countdown.
+    if (pomoState == timerOptions.STOPPED) {
+        clearInterval(interval);
+        pomoCount = 0;
+        onBreak = false;
+        currentTime(stdWork, textDisplay);
+        document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", `220 220`);
+        //Changes the color of the timer
+        document.getElementById("base-timer-path-remaining").setAttribute("stroke", "#DB2E2E");
+    }
+
+    if (--timer < -1) {
+        clearInterval(interval);
+        startStopButton.innerHTML = BEGIN_BTN_TXT; 
+        pomoState = timerOptions.STOPPED;
+        onBreak = false;
+        //Changes the color of the timer
+        document.getElementById("base-timer-path-remaining").setAttribute("stroke", "#DB2E2E");
+        currentTime(stdWork, textDisplay);
+    }
+  }, 1000);
+}
+
+
+// Helper method; calculate current time.
+function currentTime(timer, textDisplay) {
+    let minutes, seconds;
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    textDisplay.textContent = minutes + ":" + seconds;
+}
+
+function timeFraction(timer){
+    if (pomoState == timerOptions.POMO){
+        return timer/stdWork;
+    }
+    else if(pomoState == timerOptions.LONG){
+        return timer/stdExtBreak;
+    }
+    else{
+        return timer/stdBreak;
+    }
+}
+
 module.exports = { togglePomoBreak, startTimer, resetTimer };
+
+}
