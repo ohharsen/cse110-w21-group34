d80e5005ffac033fa674717322a72b52048149f7
Arsen Ohanyan <arsenohanyan@gmail.com>
2021-02-18 21:50:12 -0800

    Added Sync File Read

--- source/scripts/index.test.js
+++ source/scripts/index.test.js
@@ -1,16 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-
-const fs = require('fs')
-
-fs.readFile('../index.html', 'utf8' , (err, data) => {
-  if (err) {
-    return
-  }
-  document.body.innerHTML = data;
-})
-
 let { togglePomoBreak, startTimer, resetTimer, testDom, currentTime } = require("./index");
 
 test('checks break toggle', () => {
@@ -19,14 +9,7 @@ test('checks break toggle', () => {
 });
   
 test('checks start state', () => {
-  fs.readFile('../index.html', 'utf8' , (err, data) => {
-    if (err) {
-      return
-    }
-    document.body.innerHTML = data;
-    testDom();
-  expect(startTimer()).toStrictEqual(["pomo","✖ Reset"]);
-  });
+  expect(startTimer()).toStrictEqual([undefined, "✖ Reset"]);
 });
   
 test('checks reset state', () => {
@@ -34,26 +17,13 @@ test('checks reset state', () => {
 });
 
 test('Check current time display', ()=>{
-  fs.readFile('../index.html', 'utf8' , (err, data) => {
-    if (err) {
-      return
-    }
-    document.body.innerHTML = data;
-    testDom();
   expect(currentTime(1500, document.querySelector('#countdownText'))).toStrictEqual("25:00");
   expect(currentTime(319, document.querySelector('#countdownText'))).toStrictEqual("05:19");
   expect(currentTime(23, document.querySelector('#countdownText'))).toStrictEqual("00:23");
-  });
 });
 
-test('checks reset state', () => {
-  fs.readFile('../index.html', 'utf8' , (err, data) => {
-    if (err) {
-      return
-    }
-    document.body.innerHTML = data;
-    testDom();
-    expect(document.querySelector("title").innerText).toStrictEqual("Test Text");
-  });
+test('checks test', () => {
+  testDom();
+  expect(document.querySelector("title").innerText).toStrictEqual("Test Text");
 });
 
