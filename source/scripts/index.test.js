591d7eaf7eb6eee8c8358b2969deb77e284e99a6
Joshua Narezo <21177895+jnarezo@users.noreply.github.com>
2021-02-20 12:57:17 -0800

    Add stats frontend tests.

--- source/scripts/index.test.js
+++ source/scripts/index.test.js
@@ -21,3 +21,32 @@ test('checks test', () => {
   testDom();
   expect(document.querySelector("title").innerText).toStrictEqual("Test Text");
 });
+
+describe('statistics', () => {
+  const openStyle = 'left: calc(100vw - 25vw - 32px);';
+  const closedStyle = 'left: 100vw;';
+
+  test('if open-button opens stats-pane', () => {
+    const statsPane = document.getElementById('stats-container');
+    const openButton = document.getElementById('stats-open-button');
+
+    openButton.click();
+    expect(statsPane.getAttribute('style')).toBe(openStyle);
+  });
+  
+  test('if close-button closes stats-pane, after opening', () => {
+    const statsPane = document.getElementById('stats-container');
+    const closeButton = document.getElementById('stats-close-button');
+    
+    closeButton.click();
+    expect(statsPane.getAttribute('style')).toBe(closedStyle);
+  });
+
+  test('if open-button opens stats-pane, after closing', () => {
+    const statsPane = document.getElementById('stats-container');
+    const openButton = document.getElementById('stats-open-button');
+    
+    openButton.click();
+    expect(statsPane.getAttribute('style')).toBe(openStyle);
+  });
+});
