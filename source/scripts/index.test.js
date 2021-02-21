ff77896ebfbe12b3267923ee7e652cd18d5b526b
wis003 <57035008+wis003@users.noreply.github.com>
2021-02-21 11:48:41 -0800

    Task button functionality and interaction with local storage (#31)
    * Task button functionality and interaction with local storage
    
    * Fixed null taskButton error
    
    * Fix test errors
    
    * Add task button tests and reorder key-value pair
    
    * Change format for local storage (total task count, today task count, week task count, todays date, week start date)
    
    * Bug fixes for task button
    
    * add tests for different cases, fix week start bug
    
    * test fix
    
    Co-authored-by: Arsen Ohanyan <arsenohanyan@gmail.com>

--- source/scripts/index.test.js
+++ source/scripts/index.test.js
@@ -11,7 +11,9 @@ let {
   timerOptions, 
   beginCountdown, 
   timeFraction,
-  testDom 
+  format_date, 
+  updateLocalStorage,
+  testDom
 } = require("./index");
 
 test('checks break toggle', () => {
@@ -58,6 +60,76 @@ test('Test timer fraction', () => {
 test('checks test', () => {
   testDom();
   expect(document.querySelector("title").innerText).toStrictEqual("Test Text");
+});
+
+test('checks format date', () => {
+  let check = new Date();
+  let dd = String(check.getDate()).padStart(2, "0"); // date
+  let mm = String(check.getMonth() + 1).padStart(2, "0"); // month
+  let yyyy = check.getFullYear(); // year
+  let check_format = mm + "/" + dd + "/" + yyyy;
+  
+  expect(format_date(check)).toStrictEqual(check_format);
+});
+
+test('checks local storage test 0', () => { // test empty storage
+  let check1 = new Date(2021, 1, 18); // 2-18-2021 (month is 0-indexed)
+  let check2 = new Date(2021, 1, 18);
+  let check3 = new Date(2021, 1, 18);
+  let storage = updateLocalStorage(true, check1, check2, check3);
+  expect(storage.getItem("total-task-count")).toStrictEqual("1");
+  expect(storage.getItem("today-task-count")).toStrictEqual("1");
+  expect(storage.getItem("week-task-count")).toStrictEqual("1");
+  expect(storage.getItem("today")).toStrictEqual("02/18/2021");
+  expect(storage.getItem("week-start")).toStrictEqual("02/15/2021");
+});
+
+test('checks local storage test 1', () => { // test case 1
+  let check1 = new Date(2021, 1, 10); // 2-10-2021 (month is 0-indexed)
+  let check2 = new Date(2021, 1, 10);
+  let check3 = new Date(2021, 1, 10);
+  let storage = updateLocalStorage(true, check1, check2, check3);
+  let check4 = new Date(2021, 1, 16); // 2-16-2021 (month is 0-indexed)
+  let check5 = new Date(2021, 1, 16);
+  let check6 = new Date(2021, 1, 16);
+  storage = updateLocalStorage(false, check4, check5, check6);
+  expect(storage.getItem("total-task-count")).toStrictEqual("2");
+  expect(storage.getItem("today-task-count")).toStrictEqual("1");
+  expect(storage.getItem("week-task-count")).toStrictEqual("1");
+  expect(storage.getItem("today")).toStrictEqual("02/16/2021");
+  expect(storage.getItem("week-start")).toStrictEqual("02/15/2021"); // should update to new week start
+});
+
+test('checks local storage test 2', () => { // test case 2
+  let check1 = new Date(2021, 1, 17); // 2-17-2021 (month is 0-indexed)
+  let check2 = new Date(2021, 1, 17);
+  let check3 = new Date(2021, 1, 17);
+  let storage = updateLocalStorage(true, check1, check2, check3);
+  let check4 = new Date(2021, 1, 18); // 2-18-2021 (month is 0-indexed)
+  let check5 = new Date(2021, 1, 18);
+  let check6 = new Date(2021, 1, 18);
+  storage = updateLocalStorage(false, check4, check5, check6);
+  expect(storage.getItem("total-task-count")).toStrictEqual("2");
+  expect(storage.getItem("today-task-count")).toStrictEqual("1");
+  expect(storage.getItem("week-task-count")).toStrictEqual("2");
+  expect(storage.getItem("today")).toStrictEqual("02/18/2021");
+  expect(storage.getItem("week-start")).toStrictEqual("02/15/2021");
+});
+
+test('checks local storage test 3', () => { // test case 3
+  let check1 = new Date(2021, 1, 18); // 2-18-2021 (month is 0-indexed)
+  let check2 = new Date(2021, 1, 18);
+  let check3 = new Date(2021, 1, 18);
+  let storage = updateLocalStorage(true, check1, check2, check3);
+  let check4 = new Date(2021, 1, 18); // 2-18-2021 (month is 0-indexed)
+  let check5 = new Date(2021, 1, 18);
+  let check6 = new Date(2021, 1, 18);
+  storage = updateLocalStorage(false, check4, check5, check6);
+  expect(storage.getItem("total-task-count")).toStrictEqual("2");
+  expect(storage.getItem("today-task-count")).toStrictEqual("2");
+  expect(storage.getItem("week-task-count")).toStrictEqual("2");
+  expect(storage.getItem("today")).toStrictEqual("02/18/2021");
+  expect(storage.getItem("week-start")).toStrictEqual("02/15/2021");
 });
 
 describe('statistics', () => {
