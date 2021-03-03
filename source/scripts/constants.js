572b15ffa767d5486e13cdfbe5596c90c6d576be
jnarezo <21177895+jnarezo@users.noreply.github.com>
2021-03-03 15:17:27 -0800

    Switch to modules.

---
+++ source/scripts/constants.js
@@ -0,0 +1,30 @@
+/* Constants */
+export const START_STOP_ID = 'start-stop-button';
+export const RESET_BTN_TXT = '✖ Reset';
+export const BEGIN_BTN_TXT = '▶ Begin';
+export const TASK_BTN_ID = 'task';
+export const TOTAL_TASK_ID = 'total-task-count';
+export const TODAY_TASK_ID = 'today-task-count';
+export const WEEK_TASK_ID = 'week-task-count';
+export const TODAY_DATE_ID = 'today';
+export const WEEK_START_ID = 'week-start';
+export const TODAY_POMO_ID = 'today-pomo-count';
+export const DISTRACTION = 'total-distraction';
+export const TODAY_DISTRACTION = 'today-distraction';
+export const LENGTH_OF_WEEK = 7;
+export const stdWork = 1500; // # of seconds in a work pomo (orig. 1500)
+export const stdBreak = 300; // # of seconds in a short break (orig. 300)
+export const stdExtBreak = 900; // # of seconds in a long break (orig. 900)
+
+/**
+ * Enumerated timer states
+ * @enum {string}
+ */
+export const timerOptions = {
+  STOPPED: 'stopped',
+  POMO: 'pomo',
+  SHORT: 'short break',
+  LONG: 'long break'
+};
+
+export const localStorage = window.localStorage;
