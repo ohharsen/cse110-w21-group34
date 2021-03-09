bd2a68828a46e30e14b885faa17bcb77982cb5ed
Matteen Khanzadeh <matteenk@gmail.com>
2021-03-09 01:04:01 -0800

    fix lint

--- source/scripts/graph.js
+++ source/scripts/graph.js
@@ -20,11 +20,11 @@ export function drawGraph (canvas, data = [0, 0, 0, 0, 0, 0, 0]) {
   drawBars(ctx, canvas.height, data, axes);
 }
 
-/** 
-* @param {CanvasRenderingContext2D} ctx 
-* @param {number} canvasHeight 
-* @param {number} canvasWidth 
-* @param {number[]} axes 
+/**
+* @param {CanvasRenderingContext2D} ctx
+* @param {number} canvasHeight
+* @param {number} canvasWidth
+* @param {number[]} axes
 */
 function drawAxes (ctx, canvasHeight, canvasWidth, axes) {
   // draw axes
@@ -34,28 +34,28 @@ function drawAxes (ctx, canvasHeight, canvasWidth, axes) {
   ctx.font = TEXT_FONT;
   ctx.textAlign = 'center';
   for (const [i, label] of X_LABELS.entries()) {
-      const x = 32 + i * (BAR_WIDTH + BAR_PADDING);
-      const y = canvasHeight - 32 + 8 + (TEXT_HEIGHT / 2);
-      ctx.fillText(label, x, y);
+    const x = 32 + i * (BAR_WIDTH + BAR_PADDING);
+    const y = canvasHeight - 32 + 8 + (TEXT_HEIGHT / 2);
+    ctx.fillText(label, x, y);
   }
 }
 
 /**
-* 
+*
 * @param {CanvasRenderingContext2D} ctx The canvas' 2d rendering context
 * @param {number} canvasHeight The canvas' height
-* @param {number[]} data 
-* @param {number[]} axes 
+* @param {number[]} data
+* @param {number[]} axes
 */
-function drawBars(ctx, canvasHeight, data, axes) {
+function drawBars (ctx, canvasHeight, data, axes) {
   const maxAxis = axes[axes.length - 1];
   const maxHeight = canvasHeight - 32;
   for (const [i, d] of data.entries()) {
     const barHeight = Math.round(maxHeight * (d / maxAxis));
     if (barHeight > 0) {
-        const x = 32 + i * (BAR_WIDTH + BAR_PADDING);
-        const y = maxHeight - barHeight;
-        drawBar(ctx, x, y, BAR_WIDTH, barHeight, BAR_COLOR);
+      const x = 32 + i * (BAR_WIDTH + BAR_PADDING);
+      const y = maxHeight - barHeight;
+      drawBar(ctx, x, y, BAR_WIDTH, barHeight, BAR_COLOR);
     }
   }
 }
@@ -72,27 +72,25 @@ function calculateAxes (data) {
   //      4 - 10 => round max up 1
   //      11 - 30 => round max up 2
   // spacing = max axis / 4
-    
+
   // distribute the spacing
   const axes = [0, 1, 2, 3];
-    
-  // Calculating current max pomo cycles within week 
+
+  // Calculating current max pomo cycles within week
   let max = Math.max(...data);
 
-  // Checking max value to determine axes values 
+  // Checking max value to determine axes values
   if (max < 4) {
-      return axes;
-  }
-  else if (max < 11) {
-      max += 1;
-  }
-  else {
-      max += 2;
+    return axes;
+  } else if (max < 11) {
+    max += 1;
+  } else {
+    max += 2;
   }
 
-  // Setting axes values and rounding to one decimal place 
-  axes[1] = (max/3).toFixed(1);
-  axes[2] = (2*max/3).toFixed(1);
+  // Setting axes values and rounding to one decimal place
+  axes[1] = (max / 3).toFixed(1);
+  axes[2] = (2 * max / 3).toFixed(1);
   axes[3] = max;
 
   // Checking max value to determine axes values
@@ -123,9 +121,9 @@ function calculateAxes (data) {
  * @param {number} h height
  * @param {?string} color hex color (e.g. #fafefc)
  */
-function drawBar(ctx, x, y, w, h, color = '#000000') {
-    ctx.save();
-    ctx.fillStyle = color;
-    ctx.fillRect(x - Math.round(w/2), y, w, h);
-    ctx.restore();
+function drawBar (ctx, x, y, w, h, color = '#000000') {
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.fillRect(x - Math.round(w / 2), y, w, h);
+  ctx.restore();
 }
