5d87dc0824fd6c7acd336d121e9a593a8ff0c696
virenabhyankar <forvirenra@gmail.com>
2021-03-13 17:12:07 -0800

    Refactor task storage

--- source/scripts/util/storage.js
+++ source/scripts/util/storage.js
@@ -1,3 +1,5 @@
+import * as Constants from '../constants.js';
+
 export const BEST_DAILY_POMO_ID = 'best-daily-pomo-count';
 export const TOTAL_POMO_ID = 'total-pomo-count';
 export const TOTAL_TASK_ID = 'total-task-count';
@@ -19,15 +21,15 @@ export const TODAY_DISTRACTION = 'today-distraction';
  * @returns local storage for debug
  */
 export function updateTasks (dayCounter, weekCounter, dayOfWeek) {
-  window.localStorage.setItem(Constants.TODAY_TASK_ID, String(dayCounter));
-  window.localStorage.setItem(Constants.WEEK_TASK_ID, String(weekCounter));
+  window.localStorage.setItem(TODAY_TASK_ID, String(dayCounter));
+  window.localStorage.setItem(WEEK_TASK_ID, String(weekCounter));
 
-  const totalTasks = Number(window.localStorage.getItem(Constants.TOTAL_TASK_ID)) + 1;
-  window.localStorage.setItem(Constants.TOTAL_TASK_ID, String(totalTasks));
+  const totalTasks = Number(window.localStorage.getItem(TOTAL_TASK_ID)) + 1;
+  window.localStorage.setItem(TOTAL_TASK_ID, String(totalTasks));
 
-  const weekHistory = JSON.parse(window.localStorage.getItem(Constants.WEEK_HISTORY));
+  const weekHistory = JSON.parse(window.localStorage.getItem(WEEK_HISTORY));
   ++weekHistory[dayOfWeek];
-  window.localStorage.setItem(Constants.WEEK_HISTORY, JSON.stringify(weekHistory));
+  window.localStorage.setItem(WEEK_HISTORY, JSON.stringify(weekHistory));
   updateStats();
   return window.localStorage;
 }
@@ -35,23 +37,23 @@ export function updateTasks (dayCounter, weekCounter, dayOfWeek) {
 /**
  * Update's pomo count for today in local storage
  * @param {Number} todayPomos The number of daily current pomos completed
- * @param {String} todayStorage updatePomoCount the local storage date for the current day
+ * @param {String} todayStorage the local storage date for the current day
  * @return number of pomos completed today
  */
  export function setPomoCount (todayPomos, todayStorage) {
   // update pomo cycle day count
-  const today = formatDate(new Date());
+  const today = new Date(); // formatDate
   // case if we are on same day
   if (today === todayStorage) {
     todayPomos++;
   } else { // case if we are on different day
     todayPomos = 1;
-    window.localStorage.setItem(Constants.TODAY_DATE_ID, today);
+    window.localStorage.setItem(TODAY_DATE_ID, JSON.stringify(today));
   }
-  window.localStorage.setItem(Constants.TODAY_POMO_ID, String(todayPomos));
-  window.localStorage.setItem(Constants.TOTAL_POMO_ID, String(Number(window.localStorage.getItem(Constants.TOTAL_POMO_ID)) + 1));
-  if (Number(window.localStorage.getItem(Constants.BEST_DAILY_POMO_ID)) < todayPomos) {
-    window.localStorage.setItem(Constants.BEST_DAILY_POMO_ID, todayPomos);
+  window.localStorage.setItem(TODAY_POMO_ID, String(todayPomos));
+  window.localStorage.setItem(TOTAL_POMO_ID, String(Number(window.localStorage.getItem(TOTAL_POMO_ID)) + 1));
+  if (Number(window.localStorage.getItem(BEST_DAILY_POMO_ID)) < todayPomos) {
+    window.localStorage.setItem(BEST_DAILY_POMO_ID, todayPomos);
   }
 
   return todayPomos;
@@ -62,10 +64,10 @@ export function updateTasks (dayCounter, weekCounter, dayOfWeek) {
  */
  export function incrDailyPomoCount () {
   const dayIdx = ((new Date()).getDay() - 1) % Constants.LENGTH_OF_WEEK;
-  const weekHistory = JSON.parse(window.localStorage.getItem(Constants.WEEK_HISTORY)) || [0, 0, 0, 0, 0, 0, 0];
+  const weekHistory = JSON.parse(window.localStorage.getItem(WEEK_HISTORY)) || [0, 0, 0, 0, 0, 0, 0];
 
   weekHistory[dayIdx]++;
-  window.localStorage.setItem(Constants.WEEK_HISTORY, JSON.stringify(weekHistory));
+  window.localStorage.setItem(WEEK_HISTORY, JSON.stringify(weekHistory));
 }
 
 /**
@@ -73,7 +75,7 @@ export function updateTasks (dayCounter, weekCounter, dayOfWeek) {
  */
  export function clearWeeklyHistory () {
   const zeros = [0, 0, 0, 0, 0, 0, 0];
-  window.localStorage.setItem(Constants.WEEK_HISTORY, JSON.stringify(zeros));
+  window.localStorage.setItem(WEEK_HISTORY, JSON.stringify(zeros));
 }
 
 /**
@@ -81,7 +83,7 @@ export function updateTasks (dayCounter, weekCounter, dayOfWeek) {
  * @returns the updated number of total cycles
  */
 export function incrTotalCycles () {
-  const totalCycles = Number(window.localStorage.getItem(Constants.TOTAL_CYCLE_ID)) + 1;
+  const totalCycles = Number(window.localStorage.getItem(TOTAL_CYCLE_ID)) + 1;
   window.localStorage.setItem(TOTAL_CYCLE_ID, String(totalCycles));
   return window.localStorage.getItem(TOTAL_CYCLE_ID);
 }
@@ -98,28 +100,39 @@ export function setDistractions (todayDistractions, todayStorage) {
   window.localStorage.setItem(TOTAL_DISTRACTION, String(distractions + 1));
 
   // Today's distractions
-  const today = formatDate(new Date());
+  const today = new Date(); // formatDate()
   if (today === todayStorage) {
     todayDistractions++;
   } else {
     // Update
     todayDistractions = 1;
-    window.localStorage.setItem(TODAY_DATE_ID, today);
+    window.localStorage.setItem(TODAY_DATE_ID, JSON.stringify(today));
   }
   window.localStorage.setItem(TODAY_DISTRACTION, String(todayDistractions));
 
   return todayDistractions;
 }
 
-/**
-   * Reformat Date() variable to mm:dd:yyyy
-   * @param {Date} toFormat date object to change
-   * @returns formatted string
-   */
- export function formatDate (toFormat) {
-  const dd = String(toFormat.getDate()).padStart(2, '0'); // date
-  const mm = String(toFormat.getMonth() + 1).padStart(2, '0'); // month
-  const yyyy = toFormat.getFullYear(); // year
-  const formatted = mm + '/' + dd + '/' + yyyy;
-  return formatted;
+export function getTodayStorageDate () {
+  return new Date(JSON.parse(window.localStorage.getItem(Constants.TODAY_DATE_ID)));
+}
+
+export function setTodayStorageDate (today) {
+  window.localStorage.setItem(TODAY_DATE_ID, JSON.stringify(today));
+}
+
+export function getWeekStorageDate () {
+  return new Date(JSON.parse(window.localStorage.getItem(WEEK_START_ID)));
+}
+
+export function setWeekStorageDate (mondayDate) {
+  window.localStorage.setItem(WEEK_START_ID, JSON.stringify(mondayDate));
+}
+
+export function getWeekCounter () {
+  return Number(window.localStorage.getItem(WEEK_TASK_ID));
+}
+
+export function getDayCounter () {
+  return Number(window.localStorage.getItem(TODAY_TASK_ID));
 }
