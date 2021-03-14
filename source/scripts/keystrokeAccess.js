295d9205a5865a456b259aaf30610d369244bf09
Arsen Ohanyan <arsenohanyan@gmail.com>
2021-03-13 17:20:54 -0800

    Fix linting

--- source/scripts/keystrokeAccess.js
+++ source/scripts/keystrokeAccess.js
@@ -7,9 +7,9 @@ let keystrokeMode = true;
 /**
  * The event listener for whenever keys are pressed
  * Listens only to specific keys and lets other keys perform default action
- * @param {Event} e the fired event object 
+ * @param {Event} e the fired event object
  */
-function keyControls(e) {
+function keyControls (e) {
   if (e.code === 'ArrowLeft') {
     e.preventDefault();
     openStatsPane();
@@ -30,7 +30,7 @@ document.onkeydown = keyControls;
 /**
  * Function to toggle keystroke access. Called whenever the user toggles the setting switch
  */
-function toggleKeystroke(){
+function toggleKeystroke () {
   keystrokeMode = !keystrokeMode;
   document.onkeydown = keystrokeMode ? keyControls : undefined;
 }
