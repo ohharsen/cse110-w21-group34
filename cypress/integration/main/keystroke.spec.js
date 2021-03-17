68b287daec685b88f5d9bb4083a3763f7c00d154
jnarezo <21177895+jnarezo@users.noreply.github.com>
2021-03-16 17:21:25 -0700

    Remove unnecessary Cypress code.

--- cypress/integration/main/keystroke.spec.js
+++ cypress/integration/main/keystroke.spec.js
@@ -29,14 +29,6 @@ describe('Timer Keys Test', ()=>{
 describe('Pane Tests', ()=>{
     beforeEach(() => {
         cy.visit(Constants.HOST_ADDRESS);
-        if (window.navigator && navigator.serviceWorker) {
-            navigator.serviceWorker.getRegistrations()
-            .then((registrations) => {
-              registrations.forEach((registration) => {
-                registration.unregister()
-              })
-            })
-          }
     });
 
     it('Left Arrow Then Right Arrow toggles stats pane', () => {
