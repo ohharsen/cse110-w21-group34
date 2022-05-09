22020a0d61cd517094f55b3237f9aea749e2e7af
Jun Kim <yjk007@ucsd.edu>
2022-05-09 02:17:38 -0700

    Close to RAIL. Need to fix workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
-const sourcemaps = require('gulp-sourcemaps');
-const { src, series, parallel, dest, watch } = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const { src, parallel, dest } = require('gulp');
 
-const jsPath = 'source/scripts/**/*.js';
+const jsPath1 = 'build/bundled/main.js';
+const jsPath2 = 'build/bundled/timeWorker.js';
+const cssPath = 'source/styles/*.css';
 
 
 function copyHtml() {
@@ -16,16 +20,32 @@ function imgTask() {
   return src('source/images/*').pipe(imagemin()).pipe(gulp.dest('build/images'));
 }
 
-function jsTask() {
-  return src(jsPath)
-    .pipe(sourcemaps.init())
-    .pipe(concat('all.js'))
+function jsTask1() {
+  return src(jsPath1)
+    .pipe(concat('main.js'))
     .pipe(terser())
-    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/scripts'));
 }
 
-exports.jsTask = jsTask;
+function jsTask2() {
+  return src(jsPath2)
+    .pipe(concat('timeWorker.js'))
+    .pipe(terser())
+    .pipe(dest('build/scripts'));
+}
+
+
+function cssTask() {
+  return src(cssPath)
+    .pipe(concat('main.css'))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('build/styles'));
+}
+
+
+exports.cssTask = cssTask;
+exports.jsTask1 = jsTask1;
+exports.jsTask2 = jsTask2;
 exports.imgTask = imgTask;
 exports.copyHtml = copyHtml;
-exports.default = parallel(copyHtml, imgTask);
+exports.default = parallel(copyHtml, imgTask, cssTask);
