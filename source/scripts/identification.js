bd19c522fe811c615bfa29124c71707eff18f412
cbj99 <bac001@ucsd.edu>
2022-05-12 10:15:24 -0700

    add user identification for LogRocket

---
+++ source/scripts/identification.js
@@ -0,0 +1,8 @@
+
+LogRocket.identify('umcvob/texascodeem', {
+    name: 'Baijun Chen',
+    email: 'bac001@ucsd.edu',
+    age: 22,
+    favoriteColor: 'blue',
+    currentSubscription: 'professional-plan',
+  });
