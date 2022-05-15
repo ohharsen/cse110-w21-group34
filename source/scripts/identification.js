2ece163b6530f8355c94acb108c771def473e6fc
cbj99 <bac001@ucsd.edu>
2022-05-14 22:04:43 -0700

    delete identification.js

--- source/scripts/identification.js
+++
@@ -1,8 +0,0 @@
-
-LogRocket.identify('umcvob/texascodeem', {
-    name: 'Baijun Chen',
-    email: 'bac001@ucsd.edu',
-    age: 22,
-    favoriteColor: 'blue',
-    currentSubscription: 'professional-plan',
-  });
