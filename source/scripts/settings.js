38111e16832ddb54f67897a8615f8758a2f499b0
Jason Au <danryan808512@gmail.com>
2022-05-19 16:11:18 -0700

    Added semicolon previously causing CI/CD linting check to fail

--- source/scripts/settings.js
+++ source/scripts/settings.js
@@ -90,4 +90,4 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-})
+});
