25bbd2f6100b52d48504e294807578fadfc277f8
Jun Kim <yjk007@ucsd.edu>
2022-05-20 02:49:55 -0700

    Fixed merge conflicts. Refactored CSS import statement, and downloaded font instead. Added comments to gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,90 @@
+// Required to use gulp
 const gulp = require('gulp');
+
+// Optimize the images
 const imagemin = require('gulp-imagemin');
+
+// Replaces html to different text (Used for bundled files)
 const htmlreplace = require('gulp-html-replace');
+
+// Used to minify javascript
 const terser = require('gulp-terser');
+
+// Concats files together
 const concat = require('gulp-concat');
+
+// Used to minify CSS
 const postcss = require('gulp-postcss');
+
+// Wipes out build directory for each build
 const clean = require('gulp-clean');
+
+// Used to minify HTML
 const htmlmin = require('gulp-htmlmin');
+
+// Used for CSS minification
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
+
+// Used for running gulp/defining srcs and dests
 const { src, parallel, dest, series } = require('gulp');
 
+// Constants defining paths
 const jsPath = 'source/scripts/**/*.js'
 const cssPath = 'source/styles/*.css';
 
+// Clear the build directory
 function reset() {
-  return gulp.src('build', {read: false})
+  return src('build', {read: false})
   .pipe(clean());
 }
 
+// Copy the favicon
 function copyfavi() {
-  return src('source/*.ico').pipe(gulp.dest('build'));
+  return src('source/*.ico').pipe(dest('build'));
 }
 
+// Copy the sounds
 function copySounds() {
-  return src('source/sounds/*.mp3').pipe(gulp.dest('build/sounds'));
+  return src('source/sounds/*.mp3').pipe(dest('build/sounds'));
 }
 
+// Copy the fonts
+function copyFonts() {
+  return src('source/fonts/*').pipe(dest('build/fonts'));
+}
+
+// Copy and minify images
 function imgTask() {
-  return src('source/images/*').pipe(imagemin()).pipe(gulp.dest('build/images'));
+  return src('source/images/*').pipe(imagemin()).pipe(dest('build/images'));
 }
 
+// Copy and minify the HTML
 function htmlTask() {
-  return gulp.src('source/index.html')
+  return src('source/index.html')
+    .pipe(htmlreplace({
+    'css': 'styles/main.css',
+    }))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
+    .pipe(dest('build'));
 }
 
+// Copy and minify the Javscript
 function jsTask() {
   return src(jsPath)
     .pipe(terser())
     .pipe(dest('build/scripts'));
 }
 
+// Copy and minify the CSS
 function cssTask() {
   return src(cssPath)
+    .pipe(concat('main.css'))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest('build/styles'));
 }
 
-
+// Export the tasks into gulp so we can run them
 exports.cssTask = cssTask;
 exports.reset = reset;
 exports.htmlTask = htmlTask;
@@ -56,5 +92,6 @@ exports.jsTask = jsTask;
 exports.imgTask = imgTask;
 exports.copyfavi = copyfavi;
 exports.copySounds = copySounds;
+exports.copyFonts = copyFonts;
 
-exports.default = series(reset ,parallel(htmlTask, copyfavi, copySounds, imgTask, cssTask, jsTask));
+exports.default = series(reset ,parallel(htmlTask, copyfavi, copySounds, copyFonts, imgTask, cssTask, jsTask));
