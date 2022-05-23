ee69a1431a84f71513f27f417a3e5319303f9b87
Geewhan Kim <g1kim@ucsd.edu>
2022-05-23 00:11:32 -0700

    basic pwa support

---
+++ source/scripts/index.js
@@ -0,0 +1,12 @@
+/**
+ * Checks for service walker support
+ */
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW Registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW Registration Failed!");
+        console.log(error);
+    })
+}
