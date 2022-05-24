e40bd734c38691e257dc7bab8b25c71f637f702f
Jun Kim <yjk007@ucsd.edu>
2022-05-23 18:34:20 -0700

    Testing optimized Log Rocket build

--- source/3rd_party/logRocketInit.js
+++ source/3rd_party/logRocketInit.js
@@ -1,3 +1,3 @@
-window.addEventListener('OMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() {
   setTimeout(window.LogRocket && window.LogRocket.init('umcvob/texascodeem'), 1000);
 });
