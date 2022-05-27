6c552d074c967342b5d0b889232ff6aae6bc72c7
Geewhan Kim <g1kim@ucsd.edu>
2022-05-26 17:19:03 -0700

    code styling

--- source/sw.js
+++ source/sw.js
@@ -1,21 +1,22 @@
 /**
  * Service Worker only installs under these conditions 
  */
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./"]);
-        })
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('static').then(cache => {
+      return cache.addAll(['./']);
+    })
+  );
 });
 
 /**
  * Runs when we have fetch request  
  */
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
+
